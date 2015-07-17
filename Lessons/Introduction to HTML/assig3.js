fd6b3204686fa3c823d9b95be0da4c971f1630a0
Rituparna15 <rituparna@fusioncharts.com>
2015-07-17 11:36:35 +0530

    Assignment 4

--- Lessons/Introduction to HTML/assig3.js
+++ Lessons/Introduction to HTML/assig3.js
@@ -1,22 +1,24 @@
 
 function to_json()
-{	debugger;
+{	
 	var details=
 	{
 		"Name": "",
 		"Sex" : "",
+		"Phone":"",
 		"Email": "",
 		"Address" : "",
 		"Country" : "",
 		"Interest" : ""
 	};
 
-	if(document.getElementsByName("name")[0].value!= "" && document.getElementsByName("email")[0].value!= "" && document.getElementsByName("address")[0].value!= "" && document.getElementsByName("country").value!= "") 
+	if(document.getElementsByName("phone")[0].value!= "" && document.getElementsByName("name")[0].value!= "" && document.getElementsByName("email")[0].value!= "" && document.getElementsByName("address")[0].value!= "" && document.getElementsByName("country").value!= "") 
 	{	
 		details.Name = document.getElementsByName("name")[0].value,
 		details.Email = document.getElementsByName("email")[0].value,
+		details.Phone = document.getElementsByName("phone")[0].value
 		details.Address = document.getElementsByName("address")[0].value,
-		details.Country = document.getElementsByName("country")[0].value
+		details.Country = document.getElementsByName("country")[0].value,
 	}
 	else
 	{
@@ -52,3 +54,55 @@ function to_json()
  }
 
 to_json();
+
+function ph_validator()
+{
+	var ph = details.Phone;
+	var array = [];
+	var count = 0, x=0 ;
+	do
+	{
+		x = ph%10;
+		array.push(x);
+		ph = ph/10;
+		count = count++;
+	}
+	while(ph>0);
+
+	if(count==10)
+	{
+		if(array[count-1]!=9||array[count-1]!=8||array[count-1]!=7)
+			{
+				alert("Phone number is not a valid Indian number. Please enter again.");
+			}
+		else{
+				alert("Phone number is valid!");
+			}
+	}
+	else
+	{
+		alert("Phone number does not contain 10 digits. Please enter again.")
+	}
+
+}
+
+ph_validator;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
