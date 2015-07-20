e0a52103ef8e8fcabc883860ce944cc5dee5f897
Rituparna15 <rituparna@fusioncharts.com>
2015-07-20 12:31:26 +0530

    Weekend- 1 PRoject

---
+++ Lessons/Introduction to HTML/lightbox.js
@@ -0,0 +1,12 @@
+
+<script>
+function mOver() 
+{
+    document.getElementById("t").style.color = "green";
+}
+
+function mOut() 
+{
+	document.getElementById("t").style.color = "green";    
+}
+</script>
